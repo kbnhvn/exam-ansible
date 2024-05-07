dc372d85d710920512a53de5258b5e03ced21d95
kbnhvn <joel.lourenco.perso@gmail.com>
2024-05-07 15:34:28 +0000

    Fix molecule

--- roles/wordpress/molecule/default/tests/test_default.py
+++ roles/wordpress/molecule/default/tests/test_default.py
@@ -1,21 +1,5 @@
 """Role testing files using testinfra."""
 
-
-def test_mysql_running(host):
-    if "mysql_instance" in host.backend.get_hostname():
-        mysql = host.service("mysql")
-        assert mysql.is_running
-        assert mysql.is_enabled
-
-def test_mysql_create_table(host):
-    if "mysql_instance" in host.backend.get_hostname():
-        cmd = host.run('mysql -u %s -p%s -h %s -e "CREATE TABLE test_table(id INT);" %s',
-                    host.ansible.get_variables()['wp_mysql_user'],
-                    host.ansible.get_variables()['wp_mysql_password'],
-                    host.ansible.get_variables()['wp_mysql_host'],
-                    host.ansible.get_variables()['wp_mysql_db'])
-        assert 'ERROR' not in cmd.stderr, "Failed to create table in MySQL"
-
 def test_nginx_installed(host):
     if "wordpress_instance" in host.backend.get_hostname():
         nginx = host.package("nginx")
