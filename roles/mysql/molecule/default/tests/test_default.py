a4fe0101fcfb10652f1aee7bc80f42612720bd9e
kbnhvn <joel.lourenco.perso@gmail.com>
2024-05-08 12:55:44 +0000

    Test mysql molecule

--- roles/mysql/molecule/default/tests/test_default.py
+++ roles/mysql/molecule/default/tests/test_default.py
@@ -1,16 +1,14 @@
 """Role testing files using testinfra."""
 
 def test_mysql_running(host):
-    if "mysql_instance" in host.backend.get_hostname():
-        mysql = host.service("mysql")
-        assert mysql.is_running
-        assert mysql.is_enabled
+    mysql = host.service("mysql")
+    assert mysql.is_running
+    assert mysql.is_enabled
 
 def test_mysql_create_table(host):
-    if "mysql_instance" in host.backend.get_hostname():
-        cmd = host.run('mysql -u %s -p%s -h %s -e "CREATE TABLE test_table(id INT);" %s',
-                    host.ansible.get_variables()['wp_mysql_user'],
-                    host.ansible.get_variables()['wp_mysql_password'],
-                    host.ansible.get_variables()['wp_mysql_host'],
-                    host.ansible.get_variables()['wp_mysql_db'])
-        assert 'ERROR' not in cmd.stderr, "Failed to create table in MySQL"
+    cmd = host.run('mysql -u %s -p%s -h %s -e "CREATE TABLE test_table(id INT);" %s',
+                "datascientest",
+                "dbpassword",
+                "mysql_instance",  # Point to the MySQL container
+                "wordpress")
+    assert 'ERROR' not in cmd.stderr, "Failed to create table in MySQL"
