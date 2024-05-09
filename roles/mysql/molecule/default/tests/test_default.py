e3fe16999bf072f640cf2048f396398bc5e3b8c3
kbnhvn <joel.lourenco.perso@gmail.com>
2024-05-09 10:00:59 +0000

    Fix tasks and prepare

--- roles/mysql/molecule/default/tests/test_default.py
+++ roles/mysql/molecule/default/tests/test_default.py
@@ -9,6 +9,6 @@ def test_mysql_create_table(host):
     cmd = host.run('mysql -u %s -p%s -h %s -e "CREATE TABLE test_table(id INT);" %s',
                 "datascientest",
                 "dbpassword",
-                "mysql_instance",  # Point to the MySQL container
+                "mysql_molecule_instance",  # Point to the MySQL container
                 "wordpress")
     assert 'ERROR' not in cmd.stderr, "Failed to create table in MySQL"
--- roles/wordpress/molecule/default/tests/test_default.py
+++ roles/wordpress/molecule/default/tests/test_default.py
@@ -1,39 +1,33 @@
 """Role testing files using testinfra."""
 
 def test_nginx_installed(host):
-    if "wordpress_instance" in host.backend.get_hostname():
-        nginx = host.package("nginx")
-        assert nginx.is_installed
+    nginx = host.package("nginx")
+    assert nginx.is_installed
 
 def test_nginx_running(host):
-    if "wordpress_instance" in host.backend.get_hostname():
-        nginx = host.service("nginx")
-        assert nginx.is_running
-        assert nginx.is_enabled
+    nginx = host.service("nginx")
+    assert nginx.is_running
+    assert nginx.is_enabled
 
 def test_nginx_config(host):
-    if "wordpress_instance" in host.backend.get_hostname():
-        cmd = host.run("nginx -t")
-        assert cmd.rc == 0, "Nginx configuration test failed"
+    cmd = host.run("nginx -t")
+    assert cmd.rc == 0, "Nginx configuration test failed"
 
-def test_wp_config_exists(host):
-    if "wordpress_instance" in host.backend.get_hostname():    
-        wp_config = host.file("/var/www/html/demo.com/wp-config.php")
-        assert wp_config.exists
+def test_wp_config_exists(host): 
+    wp_config = host.file("/var/www/html/demo.com/wp-config.php")
+    assert wp_config.exists
 
 def test_mysql_connection(host):
-    if "wordpress_instance" in host.backend.get_hostname():
-        wp_db_host = host.ansible.get_variables().get('wp_db_host')
-        wp_db_user = host.ansible.get_variables().get('wp_db_user')
-        wp_db_password = host.ansible.get_variables().get('wp_db_password')
-        wp_mysql_db = host.ansible.get_variables().get('wp_mysql_db')
-        cmd = host.run(f"mysql -h {wp_db_host} -u {wp_db_user} -p{wp_db_password} -e 'SHOW DATABASES;'")
-        assert 'Error' not in cmd.stderr, "MySQL connection failed: {}".format(cmd.stderr)
-        assert wp_mysql_db in cmd.stdout, "Database {} should be listed in MySQL databases".format(wp_mysql_db)
+    wp_db_host = host.ansible.get_variables().get('wp_db_host')
+    wp_db_user = host.ansible.get_variables().get('wp_db_user')
+    wp_db_password = host.ansible.get_variables().get('wp_db_password')
+    wp_mysql_db = host.ansible.get_variables().get('wp_mysql_db')
+    cmd = host.run(f"mysql -h {wp_db_host} -u {wp_db_user} -p{wp_db_password} -e 'SHOW DATABASES;'")
+    assert 'Error' not in cmd.stderr, "MySQL connection failed: {}".format(cmd.stderr)
+    assert wp_mysql_db in cmd.stdout, "Database {} should be listed in MySQL databases".format(wp_mysql_db)
 
 def test_wordpress_accessible(host):
-    if "wordpress_instance" in host.backend.get_hostname():
-        response = host.run("curl -sL -w '%{http_code}\\n' 'http://localhost/' -o /dev/null")
-        assert response.stdout.strip() == '200', "WordPress is not accessible or not returning HTTP 200"
+    response = host.run("curl -sL -w '%{http_code}\\n' 'http://localhost/' -o /dev/null")
+    assert response.stdout.strip() == '200', "WordPress is not accessible or not returning HTTP 200"
 
 
