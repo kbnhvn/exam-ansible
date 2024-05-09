8cd908d114cdf6a22bf4d964f8a58823ebd7c1e6
kbnhvn <joel.lourenco.perso@gmail.com>
2024-05-09 11:13:10 +0000

    Fix tests

--- roles/wordpress/molecule/default/tests/test_default.py
+++ roles/wordpress/molecule/default/tests/test_default.py
@@ -17,17 +17,3 @@ def test_wp_config_exists(host):
     wp_config = host.file("/var/www/html/demo.com/wp-config.php")
     assert wp_config.exists
 
-def test_mysql_connection(host):
-    wp_db_host = host.ansible.get_variables().get('wp_db_host')
-    wp_db_user = host.ansible.get_variables().get('wp_db_user')
-    wp_db_password = host.ansible.get_variables().get('wp_db_password')
-    wp_mysql_db = host.ansible.get_variables().get('wp_db_name')
-    cmd = host.run(f"mysql -h {wp_db_host} -P 3306 -u {wp_db_user} -p{wp_db_password} -e 'SHOW DATABASES;'")
-    assert 'Error' not in cmd.stderr, "MySQL connection failed: {}".format(cmd.stderr)
-    assert wp_mysql_db in cmd.stdout, "Database {} should be listed in MySQL databases".format(wp_mysql_db)
-
-def test_wordpress_accessible(host):
-    response = host.run("curl -sL -w '%{http_code}\\n' 'http://localhost/' -o /dev/null")
-    assert response.stdout.strip() == '200', "WordPress is not accessible or not returning HTTP 200"
-
-
